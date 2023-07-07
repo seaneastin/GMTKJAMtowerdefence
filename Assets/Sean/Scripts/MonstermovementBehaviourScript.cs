21f41908faf111b281925aa4b7d1d602ae8206f4
Sean Eastin <sean.eastin@gmail.com>
2023-07-07 14:08:57 -0500

    made AItest

---
+++ Assets/Sean/Scripts/MonstermovementBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonstermovementBehaviourScript : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
