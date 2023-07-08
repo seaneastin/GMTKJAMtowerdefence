a62cb17f580e6fb4504408492f933fadfeacb863
Sean Eastin <sean.eastin@gmail.com>
2023-07-07 20:58:29 -0500

    added ragdol test for goblin
    removed a lot of standard assets that will not be used
    
    added ragdoll test for goblins
    
    and added test for camera system (currently wip)

---
+++ Assets/Scripts/toRagdollBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toRagdollBehavior : MonoBehaviour
+{
+    public GameObject character;
+    Animator animator;
+    CharacterController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = character.GetComponent<Animator>();
+        characterController = character.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ragdoll()
+    {
+        characterController.enabled = false;
+        animator.enabled = false;
+    }
+}
---
+++ Assets/Sean/Scripts/testmonsterdeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testmonsterdeath : MonoBehaviour
+{
+    public toRagdollBehavior ragdoll;
+    public bool Isdead;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Isdead)
+        {
+            ragdoll.ragdoll();
+        }
+    }
+}
